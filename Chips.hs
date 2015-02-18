be23e35e8c6df575d0b0e15b65f66f6a19152413
Jeyl <jenny@kermland.de>
2015-02-18 15:37:35 +0100

    Aufzaehlung der Chips vereinfacht

--- Chips.hs
+++ Chips.hs
@@ -8,14 +8,15 @@ instance Show Chip where
 
 --Anfangschips fuer jeden Spieler im Gesamtwert 4000
 chips :: [Chip]
-chips = [Chip 5,Chip 5,Chip 5,Chip 5,Chip 5,Chip 5,Chip 5,Chip 5,Chip 5,Chip 5,Chip 5,Chip 5,Chip 5,Chip 5,Chip 5,Chip 5, -- 16x5 = 80
-         Chip 10, Chip 10, Chip 10, Chip 10, Chip 10, Chip 10, Chip 10, Chip 10, Chip 10, Chip 10,
-         Chip 10, Chip 10, Chip 10, Chip 10, Chip 10, Chip 10, Chip 10, Chip 10, Chip 10, Chip 10, -- 20x10 = 200
-         Chip 20, Chip 20, Chip 20, Chip 20, Chip 20, Chip 20, Chip 20, Chip 20, Chip 20, Chip 20, Chip 20, --11x20 = 220
-         Chip 50, Chip 50, Chip 50, Chip 50, Chip 50, Chip 50, Chip 50, Chip 50, Chip 50, Chip 50, --10x50 = 500
-         Chip 100, Chip 100, Chip 100, Chip 100, Chip 100, Chip 100, Chip 100, Chip 100,-- 8x100 = 800
-         Chip 200, Chip 200, Chip 200, Chip 200, Chip 200, Chip 200, -- 6x200 = 1200
-         Chip 500, Chip 500] --2x500 = 1000
+chips = a ++ b ++ c ++ d ++ e ++ f ++ g 
+
+a = replicate 16 (Chip 5)  --16x5  = 80
+b = replicate 20 (Chip 10) --20x10 = 200
+c = replicate 11 (Chip 20) --11x20 = 220
+d = replicate 10 (Chip 50) --10x50 = 500
+e = replicate 8 (Chip 100) --8x100 = 800
+f = replicate 6 (Chip 200) --6x200 = 1200
+g = replicate 2 (Chip 500) --2x500 = 1000
 
 --Wert einer Summe von Chips
 sum :: [Chip] -> Int
@@ -26,7 +27,7 @@ change :: Chip -> [Chip]
 change (Chip 500) = [Chip 200, Chip 100, Chip 100, Chip 50, Chip 50]
 change (Chip 200) = [Chip 100, Chip 50, Chip 20, Chip 20, Chip 10]
 change (Chip 100) = [Chip 50, Chip 20, Chip 20, Chip 10]
-change (Chip 50) = [Chip 20, Chip 10, Chip 10, Chip 10]
-change (Chip 20) = [Chip 10, Chip 10]
-change (Chip 10) = [Chip 5, Chip 5]
+change (Chip 50) = replicate 3 (Chip 10) ++ [Chip 20]
+change (Chip 20) = replicate 2 (Chip 10)
+change (Chip 10) = replicate 2 (Chip 5)
 change c = [c]
