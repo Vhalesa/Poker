96c48aaa1962079a6c9287098b46c2cad8343e10
Jeyl <jenny@kermland.de>
2015-02-24 11:43:37 +0100

    benoetige Methoden in die Games.hs reingeschrieben

--- Game.hs
+++ Game.hs
@@ -3,4 +3,31 @@ module Game where
 import Cards
 import Chips
 
+main = do
+        
 
+--alle Methoden, die fuer den Spielablauf benoetigt werden
+
+--Gibt gemischtes Kartendeck aus (im Moment auf die Kommandozeile)
+mischen = do
+  randomNum <- randomIO :: IO Int 
+  let generator = makeGenerator randomNum
+  let mixedDeck = shuffle cards generator []
+  print mixedDeck
+
+-- Chipshaufen zuweisen?
+
+-- Small und Big Blind zuweisen
+
+-- Runde (ohne Kartenaufdecken)
+-- setzen, erhoehen....
+runde = undefined
+
+-- Rund1,2,3,4 (jeweils das Karteaufdecken + Aufruf von runde)
+-- mit Ausgabe, welche Karte gezogen wurde
+
+-- Showdown
+
+-- Entscheidung: weiterspielen oder aufhoeren?
+
+-- Abfrage bei menschlichen Spieler fuer jede Wettrunde/Runde etc.
